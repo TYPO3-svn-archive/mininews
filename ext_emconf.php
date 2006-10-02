e6d06df0558b34abec1c087339dda7ad979b728f
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2006-10-02 14:44:31 +0000

    Released version 1.0.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mininews/trunk@3819 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "mininews"
 #
-# Auto generated 02-10-2006 17:26
+# Auto generated 02-10-2006 17:38
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -24,12 +24,12 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => 0,
 	'createDirs' => 'uploads/tx_mininews/rte/',
 	'modify_tables' => '',
-	'clearCacheOnLoad' => 1,
+	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'author' => 'Kasper Skaarhoj',
-	'author_email' => 'kasper@typo3.com',
-	'author_company' => 'Curby Soft Multimedia',
-	'version' => '0.1.7',
+	'author' => 'Dmitry Dulepov',
+	'author_email' => 'dmitry@typo3.org',
+	'author_company' => '',
+	'version' => '1.0.0',
 	'_md5_values_when_last_written' => 'a:31:{s:9:"ChangeLog";s:4:"ba77";s:12:"ext_icon.gif";s:4:"cb2b";s:17:"ext_localconf.php";s:4:"fdc3";s:14:"ext_tables.php";s:4:"2fa2";s:14:"ext_tables.sql";s:4:"bcff";s:28:"ext_typoscript_editorcfg.txt";s:4:"8d57";s:24:"ext_typoscript_setup.txt";s:4:"aab8";s:15:"flexform_ds.xml";s:4:"292f";s:25:"icon_tx_mininews_news.gif";s:4:"cb2b";s:28:"icon_tx_mininews_news__h.gif";s:4:"3198";s:29:"icon_tx_mininews_news__ht.gif";s:4:"2a26";s:30:"icon_tx_mininews_news__htu.gif";s:4:"e737";s:29:"icon_tx_mininews_news__hu.gif";s:4:"60bd";s:28:"icon_tx_mininews_news__t.gif";s:4:"3bf1";s:29:"icon_tx_mininews_news__tu.gif";s:4:"4f7b";s:28:"icon_tx_mininews_news__u.gif";s:4:"a75d";s:28:"icon_tx_mininews_news__x.gif";s:4:"694e";s:13:"locallang.xml";s:4:"9e71";s:16:"locallang_db.xml";s:4:"0e98";s:7:"tca.php";s:4:"baf2";s:26:"template_datastructure.xml";s:4:"ea7f";s:30:"template_datastructure_arc.xml";s:4:"b560";s:14:"doc/manual.sxw";s:4:"9511";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:29:"pi1/class.tx_mininews_pi1.php";s:4:"2bd3";s:37:"pi1/class.tx_mininews_pi1_wizicon.php";s:4:"49d5";s:17:"pi1/locallang.xml";s:4:"aca1";s:12:"res/more.gif";s:4:"2c34";s:28:"template/mapped_template.t3d";s:4:"e7f0";s:32:"template/mininews_stylesheet.css";s:4:"a24e";s:31:"template/mininews_template.html";s:4:"04e4";}',
 	'constraints' => array(
 		'depends' => array(
